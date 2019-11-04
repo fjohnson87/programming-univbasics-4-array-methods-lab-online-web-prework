7d397cbf26a5f9224c64a9be455950278c43bd54
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-04 18:51:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,13 +17,13 @@ def using_reverse(array)
 end
 
 def using_first(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_last(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def using_size(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
